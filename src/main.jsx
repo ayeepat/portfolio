2667032051f906e7d9cc804f0dfee87da2163660
ayeepat <ermddan@gmail.com>
2026-04-19 14:19:15 +0300

    add vercel analytics tracking

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";   // ← NEW IMPORT
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Analytics />                                      {/* ← NEW COMPONENT */}
   </React.StrictMode>
 );
